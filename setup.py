aeb97596da71bbd1294e3266f58e569768774ae1
Jordan Milne <jmilne@uber.com>
2016-08-09 16:27:52 -0700

    Open-source release of h1-python

---
+++ setup.py
@@ -0,0 +1,72 @@
+# Copyright (c) 2016 Uber Technologies, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from __future__ import print_function
+
+import re
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+
+with open('h1/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+
+def read_long_description(filename="README.rst"):
+    with open(filename) as f:
+        return f.read().strip()
+
+
+def requirements(filename="requirements.in"):
+    with open(filename) as f:
+        return f.readlines()
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+setup(
+    author='Uber Product Security',
+    description='A HackerOne API client for Python',
+    include_package_data=True,
+    install_requires=requirements(),  # Edit these in `requirements.in`.
+    tests_require=requirements('requirements-test.in'),
+    cmdclass={'test': PyTest},
+    license='MIT',
+    long_description=read_long_description(),
+    name='h1',
+    packages=find_packages(),
+    url='gitolite@code.uber.internal:engsec/hackerone_python',
+    version=version,
+    zip_safe=False,
+    classifiers=['Private :: Do Not Upload']
+)
